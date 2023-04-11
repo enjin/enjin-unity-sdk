6254542710caff98b357a48fecddeaae4d8cee30
Bishop S Johnson <bishop@enjin.io>
2023-04-11 14:05:42 -0700

    - Bumped SDK version to 2.0.0-beta.3 - Updated CHANGELOG.md

--- Assets/Enjin/Runtime/Sdk/AssemblyInfo.cs
+++ Assets/Enjin/Runtime/Sdk/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿[assembly: System.Reflection.AssemblyCompany("Enjin Pte. Ltd.")]
 [assembly: System.Reflection.AssemblyTitle("Enjin Unity SDK")]
-[assembly: System.Reflection.AssemblyInformationalVersion("2.0.0-beta.1")]
+[assembly: System.Reflection.AssemblyInformationalVersion("2.0.0-beta.3")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Enjin.Sdk.Tests")]
